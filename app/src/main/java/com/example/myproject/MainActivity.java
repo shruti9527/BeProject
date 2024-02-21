2e5be3e3e6f8295b0ea776d7e042da443d61fcc3
shruti9527 <shrutideshpande2020.it@mmcoe.edu.in>
2024-02-21 12:00:57 +0530

    Resolved conflicts

---
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.loadUrl("file:///android_asset/index.html");
+        webView.getSettings().setJavaScriptEnabled(true);
+    }
+}
